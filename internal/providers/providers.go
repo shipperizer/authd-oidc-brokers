dabf93ff7eed257d891311c3a8f877a8fc90202b
shipperizer <alexcabb@gmail.com>
2024-11-03 21:21:23 +0000

    feat: introduce CoreConfig to accomodate special cases in the provider where no autodiscovery is possible

--- internal/providers/providers.go
+++ internal/providers/providers.go
@@ -11,6 +11,7 @@ import (
 
 // ProviderInfoer defines provider-specific methods to be used by the broker.
 type ProviderInfoer interface {
+	CoreConfig() *oidc.Provider
 	AdditionalScopes() []string
 	AuthOptions() []oauth2.AuthCodeOption
 	CheckTokenScopes(token *oauth2.Token) error
